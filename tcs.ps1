1ec2a72cf142f7edf8aba19db694c2db688a2184
lukemoore66 <33060484+lukemoore66@users.noreply.github.com>
2022-10-19 11:18:54 +0800

    Missing torrents now removed automatically
    Any torrents that do not exist are now removed from the input list automatically. Fixed some formatting issues.

--- tcs.ps1
+++ tcs.ps1
@@ -1,5 +1,5 @@
 Param (
-	[ValidateScript({Test-Path -LiteralPath $_})] [String] $InputListPath = ($PSScriptRoot + '\tcsi.txt'),
+	[ValidateScript({Test-Path -LiteralPath $_})] [String] $InputListPath = ('{0}\tcsi.txt' -f $PSScriptRoot),
 	[switch] $ExitIfEmpty
 )
 
@@ -12,73 +12,73 @@ If ($objOtherProcess) {Stop-Process -Id $PID}
 $Script:strCsLibPath = '{0}\cslib.ps1' -f $PSScriptRoot
 $Script:strCsPath = '{0}\cs.ps1' -f $PSScriptRoot
 
-#Set Constant Variables
+#set constant variables
 $INIPathDefault = '{0}\ini\rss-default.ini' -f $PSScriptRoot
 $OutputLogPath = '{0}\tcso.txt' -f $PSScriptRoot
 $InputFormats = @('.m4v', '.vob', '.avi', '.flv', '.wmv', '.ts', '.m2ts', '.avs', '.mov', '.mkv', '.mp4', '.webm', '.ogm', '.mpg', '.mpeg')
 
-#Import Functions
+#import functions
 . $strCsLibPath
 
-#Make Sure The Default Rule File Exists
+#make sure the default rule file exists
 If (!(Test-Path -LiteralPath $INIPathDefault)) {
 	Throw ("Default ini file '{0}' does not exist." -f $INIPathDefault)
 }
 	
-$objInputList = [System.Collections.Generic.List[String]]::New()
+$objTorrentList = [System.Collections.Generic.List[String]]::New()
 
-#Get All Files Within Folders For Input List
-Get-Content -LiteralPath $InputListPath | ForEach-Object {
-	If ($_.Trim()) {
-		$arrLine = $_.Split('|')
-		#First Check If File Exists
-		If (Test-Path -LiteralPath $arrLine[0]) {
-			If ((Get-Item -LiteralPath $arrLine[0]).PSIsContainer) {
-				Get-ChildItem -LiteralPath $arrLine[0] -Recurse -File | Where-Object {$InputFormats -Contains $_.Extension} | ForEach-Object {
-					$strLine = '{0}|{1}' -f $_.FullName, $arrLine[1]
-					$objInputList.Add($strLine)
-				}
-			}
-			Else {
-				$strLine = '{0}|{1}' -f $arrLine[0], $arrLine[1]
-				$objInputList.Add($strLine)
-			}
-		}
-		#Otherwise The Input File Does Not Exist
-		Else {
-			Write-Warning ("Input path: '{0}' at line {1} in '{2}' does not exist, skipping." -f $arrLine[0], $intLineCounter, $InputListPath)
-		}
+#get all files within folders for torrent input list
+$intLineCounter = 1
+ForEach ($strLine in (Get-Content -LiteralPath $InputListPath)) {
+	#skip if the line is blank
+	If (-not $strLine.Trim()) {
+		$intLineCounter++
+		Continue
 	}
+	
+	#seperate the torrent path from the ini
+	$arrLine = $strLine.Split('|')
+	
+	#skip if torrent DNE
+	If (-not (Test-Path -LiteralPath $arrLine[0])) {
+		Write-Warning ("Input path: '{0}' at line {1} in '{2}' does not exist, Removing." -f $arrLine[0], $intLineCounter, $InputListPath)
+		$intLineCounter++
+		Continue
+	}
+	
+	#if the torrent is a folder
+	If ((Get-Item -LiteralPath $arrLine[0]).PSIsContainer) {
+		#get the video files and add to the torrent list
+		Get-ChildItem -LiteralPath $arrLine[0] -Recurse -File | Where-Object {$InputFormats -Contains $_.Extension} | ForEach-Object {
+			$strLine = '{0}|{1}' -f $_.FullName, $arrLine[1]
+			$objTorrentList.Add($strLine)
+		}
+		
+		$intLineCounter++
+		Continue
+	}
+	
+	#otherwise, the torrent is a file
+	$objTorrentList.Add($strLine)
+
+	$intLineCounter++
 }
 
-#Write New Input List To Input File
-$objInputList | Set-Content -LiteralPath $InputListPath -Force
+#write new input list to input file
+Set-Content -LiteralPath $InputListPath -Value $objTorrentList
 
-#Declare Input List
+#declare input list
 $objInputList = [Ordered]@{}
 
-#Get Input List
+#build input list
 Get-Content -LiteralPath $InputListPath | % {
-	If ($_.Trim()) {
-		$arrLine = $_.Split('|')
-		#First Check If File Exists
-		If (Test-Path -LiteralPath $arrLine[0]) {
-			$objInputList.Add($arrLine[0], (Get-INIPath $arrLine[0] $arrLine[1]))
-		}
-		#Otherwise The Input File Does Not Exist
-		Else {
-			Write-Warning ("Input file: '{0}' at line {1} in '{2}' does not exist, skipping." -f $arrLine[0], $intLineCounter, $InputListPath)
-		}
-	}
+	$arrLine = $_.Split('|')
+	$objInputList.Add($arrLine[0], (Get-INIPath $arrLine[0] $arrLine[1]))
 }
 
-#Loop Through Input List
-$intFileCount = 1
-$intTotalFileCount = $objInputList.Count
-
-#Show An Error If The Input List Is Empty
+#show an error if the input list is empty
 If ($objInputList.Count -lt 1) {
-	Write-Warning ("Input list: '" + $InputListPath + "' is empty.`n")
+	Write-Warning ("Input list: '{0}' is empty. Exiting.`n" -f $InputListPath)
 	
 	If ($ExitIfEmpty) {
 		Start-Sleep 3
@@ -86,26 +86,31 @@ If ($objInputList.Count -lt 1) {
 	}
 }
 
+#loop through input list
+$intFileCount = 1
+$intTotalFileCount = 
 $objInputList.GetEnumerator() | % {
-	#Show Progress
-	$strProgress = "`nProcessing File $intFileCount of " + $intTotalFileCount
+	#show progress
+	$strProgress = "`nProcessing File {0} of {1}" -f $intFileCount, $objInputList.Count
 	$strLines = "`n" + ("=" * $strProgress.Length)
-	Write-Host -ForegroundColor Cyan ($strLines.SubString(1, $strLines.Length - 1) + $strProgress + $strLines)
+	Write-Host -ForegroundColor Cyan ($strLines + $strProgress + $strLines)
 	
-	#Run csmod
+	#run csmod
 	& $strCsPath -InputPath $_.Key -INIPath $_.Value
 	
-	#Assign Current Input List Line
+	#assign current input list line
 	$strInputListLine = $_.Key + '|' + (Get-Item -LiteralPath $_.Value).BaseName
 	
-	#Remove Current Input Line From Input List
+	#remove current input line from input list
 	Set-Content -LiteralPath $InputListPath -Value (Get-Content -LiteralPath $InputListPath | Where-Object {$_ -iNotMatch [Regex]::Escape($strInputListLine)})
 	
-	#Write Current Input Line To The Output Log
+	#write current input line to the output log
 	Add-Content -LiteralPath $OutputLogPath -Value ((Get-Date).ToString() + '|' + $strInputListLine)
 	
-	#Write An Empty Line
-	If ($intTotalFileCount -lt $objInputList.Count) {''}
+	#write an empty line
+	If ($intTotalFileCount -lt $objInputList.Count) {
+		''
+	}
 	
 	$intFileCount++
 }
